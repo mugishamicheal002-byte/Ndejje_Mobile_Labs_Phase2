004619b6bedf74bb110d71e08f8887e7ab8968db
mugishamicheal002 <mugishamicheal002@gmail.com>
2026-04-08 09:23:29 +0300

    Added_hoisted_input

--- app/src/main/java/com/ndejje/momocalc/MainActivity.kt
+++ app/src/main/java/com/ndejje/momocalc/MainActivity.kt
@@ -4,9 +4,11 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
@@ -58,6 +60,32 @@ fun BrokenInput(){
 
         }
     )
+}
+@Composable
+fun HoistedAmountInput(
+    amount: String,
+    onAmountChange: (String) -> Unit,
+    isError: Boolean = false
+){
+    Column() {
+        TextField(
+            value = amount,
+            onValueChange = onAmountChange,
+            label = {
+                Text(stringResource(R.string.enter_amount))
+            },
+            supportingText = {
+                if (isError) {
+                    Text(
+                        text = stringResource(R.string.error_numbers_only),
+                        color = MaterialTheme.colorScheme.error,
+                        style = MaterialTheme.typography.bodySmall
+                    )
+                }
+            }
+        )
+
+    }
 }
 @Preview
 @Composable
