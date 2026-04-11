f7b7ea3b64696169fde8a2f1c7b5217f97b40fb4
mugishamicheal002 <mugishamicheal002@gmail.com>
2026-04-11 20:38:36 +0300

    aded momo screen Composable

--- app/src/main/java/com/ndejje/momocalc/MainActivity.kt
+++ app/src/main/java/com/ndejje/momocalc/MainActivity.kt
@@ -5,7 +5,9 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.material3.MaterialTheme
@@ -21,6 +23,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.ndejje.momocalc.ui.theme.MoMoCalculatorAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -33,7 +36,38 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
+@Composable
+fun MoMoCalcScreen(){
+    var amountInput by remember { mutableStateOf("") }
 
+    val numericAmount = amountInput.toDoubleOrNull()
+    val isError = amountInput.isNotEmpty() && numericAmount == null
+    val fee = (numericAmount ?: 0.0) * 0.03
+    val formattedFee = "UGX %,.0f".format(fee)
+
+    Column(modifier = Modifier.padding(16.dp)) {
+        ->
+        Text(
+            text = stringResource(R.string.app_name),
+            style = MaterialTheme.typography.headlineMedium
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+
+        HoistedAmountInput(
+            amount = amountInput,
+            onAmountChange = { amountInput = it },
+            isError = isError
+        )
+        Spacer(modifier = Modifier.height(12.dp))
+
+        Text(
+            text = stringResource(R.string.fee_label, formattedFee),
+            style = MaterialTheme.typography.bodyLarge
+        )
+    }
+
+
+}
 
 @Composable
 fun HoistedAmountInput(
@@ -60,24 +94,4 @@ fun HoistedAmountInput(
         )
 
     }
-}
-@Preview
-@Composable
-fun PreviewEmptyInput() {
-     HoistedAmountInput(amount = "", onAmountChange = {})
-
-}
-
-@Preview(showBackground = true)
-@Composable
-fun PreviewFilled(){
-    HoistedAmountInput(amount = "50000", onAmountChange = {})
-}
-
-@Preview(showBackground = true)
-@Composable
-fun PreviewError(){
-    HoistedAmountInput(amount = "micheal", onAmountChange = {}, isError = true)
-
-
 }
